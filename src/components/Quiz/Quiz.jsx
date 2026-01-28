96c3d882951fa1d0aa70f02ac4e35980ce67f018
Vlad Kononiuk <vlad.kononuikc@gmail.com>
2026-01-28 21:40:39 +0100

    mobile bug fix#2

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -5,29 +5,30 @@ const Quiz = (props) => {
   const percentage = Math.round((step / total) * 100);
   return (
     <>
-      <div className="progress">
-        <div
-          style={{ width: `${percentage}%` }}
-          className="progress__inner"
-        ></div>
-      </div>
-
-      <div className="quiz fade-in" key={step}>
-        <h1 className="quiz__title">{questionData.question}</h1>
-        <div className="quiz__img ">
-          <img src={questionData.image} alt="" />
+      <div className="quiz-wrapper">
+        <div className="progress">
+          <div
+            style={{ width: `${percentage}%` }}
+            className="progress__inner"
+          ></div>
         </div>
-        <ul className="quiz__items">
-          {questionData.options.map((text) => (
-            <li
-              className="quiz__item"
-              onClick={() => onClickVariant(text)}
-              key={`${questionData.id}-${text}`}
-            >
-              {text}
-            </li>
-          ))}
-        </ul>
+        <div className="quiz fade-in" key={step}>
+          <h1 className="quiz__title">{questionData.question}</h1>
+          <div className="quiz__img ">
+            <img src={questionData.image} alt="" />
+          </div>
+          <ul className="quiz__items">
+            {questionData.options.map((text) => (
+              <li
+                className="quiz__item"
+                onClick={() => onClickVariant(text)}
+                key={`${questionData.id}-${text}`}
+              >
+                {text}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </>
   );
--- src/pages/loadingPage/LoadingPage.jsx
+++ src/pages/loadingPage/LoadingPage.jsx
@@ -2,23 +2,31 @@ import { useState, CSSProperties } from "react";
 import { ClipLoader, MoonLoader } from "react-spinners";
 
 const override = {
-  display: "block",
-  margin: "0 auto",
-  borderColor: "red",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  margin: "",
 };
 
 const LoadingPage = () => {
   let [loading, setLoading] = useState(true);
   let [color, setColor] = useState("#4facfe");
   return (
-    <MoonLoader
-      color={color}
-      loading={loading}
-      cssOverride={override}
-      size={150}
-      aria-label="Loading Spinner"
-      data-testid="loader"
-    />
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+        position: "fixed",
+        top: 0,
+        left: 0,
+        background: "rgba(255, 255, 255, 0.8)",
+      }}
+    >
+      <MoonLoader color="#4facfe" loading={true} size={150} />
+    </div>
   );
 };
 
