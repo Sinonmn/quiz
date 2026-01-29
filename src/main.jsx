1b3bc8d1826361f1c13e8c5e793ff7244141ae17
Vlad Kononiuk <vlad.kononuikc@gmail.com>
2026-01-29 20:50:05 +0100

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import ErrorPage from "./pages/errorPage/ErrorPage.jsx";
 import App from "./App.jsx";
 import Main from "./pages/Main/Main.jsx";
 
-
 const router = createBrowserRouter([
   {
     path: "/",
@@ -17,6 +16,9 @@ const router = createBrowserRouter([
     path: "/quiz",
     element: <App />,
     errorElement: <ErrorPage />,
+  },
+  {
+    basename: import.meta.env.DEV ? "/" : "/quiz",
   },
 ]);
 
