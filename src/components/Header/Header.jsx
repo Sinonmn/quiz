3ababc611e169491eec427be615eac4dabc16d40
Vlad Kononiuk <vlad.kononuikc@gmail.com>
2026-02-07 22:23:00 +0100

    fix#2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,13 @@
 import { Link } from "react-router";
 import "./Header.css";
+import logo from "../../assets/logo/favicon.png";
 
 export default function Header() {
   return (
     <header className="header">
       <div className="header__container">
         <Link to="/" className="header__logo">
-          <img
-            src="../../assets/logo/favicon.png"
-            alt="Logo"
-            className="header__logo-img"
-          />
+          <img src={logo} alt="Logo" className="header__logo-img" />
         </Link>
         <nav className="header__nav">
           <Link to="/quiz" className="header__link">
