257501799ef04c73b5eab9a06eeedcd85f4aaad7
Vlad Kononiuk <vlad.kononuikc@gmail.com>
2026-03-16 18:10:10 +0100

    remove ghost import

--- src/pages/CreateQuiz/CreateQuiz.jsx
+++ src/pages/CreateQuiz/CreateQuiz.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import "./CreateQuiz.css";
-import "react-responsive-modal/styles.css";
 
 const CreateQuiz = () => {
   const [slides, setSlides] = useState([
