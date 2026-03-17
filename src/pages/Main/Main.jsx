b200966b6870574312abcb4418474405eb6c3656
Vlad Kononiuk <vlad.kononuikc@gmail.com>
2026-03-17 19:37:15 +0100

    feat: add quiz navigator by id

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -3,21 +3,60 @@ import Footer from "../../components/Footer/Footer.jsx";
 import "./Main.css";
 import { Link } from "react-router";
 import Slider from "../../components/Slider/Slider.jsx";
+import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 const Main = () => {
+  const [quizId, setQuizId] = useState("");
+
+  const navigateToQuiz = (event) => {
+    event.preventDefault();
+    const trimmedId = quizId.trim();
+    if (!trimmedId) {
+      toast.error("Check your slides! Missing question or correct answer.", {
+        duration: 3000,
+        position: "top-right",
+
+        style: {
+          backgroundColor: "#ff4b4b",
+          color: "#fff",
+          fontWeight: "bold",
+          borderRadius: "8px",
+        },
+        iconTheme: {
+          primary: "#fff",
+          secondary: "#ff4b4b",
+        },
+      });
+    }
+    window.location.href = `https://quiz-ashen-pi.vercel.app/quiz/${trimmedId}`;
+  };
   return (
     <>
+      <Toaster />
       <div className="app-container">
         <Header />
         <main className="main">
-          <div className="main__content">
+          <div className="main__content main-content">
             <p>Try To Complete This Quiz</p>
-            <Link className="main-content__link" to="/quiz">
-              Cat Quiz
-            </Link>
-            <Link className="main-content__link" to="/createQuiz">
-              Create Your Own Quiz
-            </Link>
+            <div className="main-content__links">
+              <Link className="main-content__link" to="/quiz">
+                Cat Quiz
+              </Link>
+              <Link className="main-content__link" to="/createQuiz">
+                Create Your Own Quiz
+              </Link>
+            </div>
+            <form className="main-form" onSubmit={navigateToQuiz}>
+              <input
+                className="main-content__link"
+                type="text"
+                placeholder="Select Your Id"
+                value={quizId}
+                onChange={(e) => setQuizId(e.target.value)}
+              />
+              <button className="main-form__button">Go</button>
+            </form>
           </div>
           <div className="main__slider">
             <Slider />
